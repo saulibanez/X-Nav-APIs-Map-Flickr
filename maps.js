a05af92e3b4ecb9f61ff21656fa2a5200d98c644
saulibanez <saul.ibanez.cerro@gmail.com>
2016-04-21 13:18:42 +0200

    map flickr v1

---
+++ maps.js
@@ -0,0 +1,91 @@
+var map;
+var feature;
+
+function load_map() {
+	map = new L.Map('map', {zoomControl: true});
+
+	var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+		osmAttribution = 'Map data &copy; 2012 <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+		osm = new L.TileLayer(osmUrl, {maxZoom: 18, attribution: osmAttribution});
+
+	map.setView(new L.LatLng(51.538594, -0.198075), 12).addLayer(osm);
+}
+
+function chooseAddr(lat1, lng1, lat2, lng2, osm_type) {
+	var loc1 = new L.LatLng(lat1, lng1);
+	var loc2 = new L.LatLng(lat2, lng2);
+	var bounds = new L.LatLngBounds(loc1, loc2);
+
+	if (feature) {
+		map.removeLayer(feature);
+	}
+	if (osm_type == "node") {
+		feature = L.circle( loc1, 25, {color: 'green', fill: false}).addTo(map);
+		map.fitBounds(bounds);
+		map.setZoom(18);
+	} else {
+		var loc3 = new L.LatLng(lat1, lng2);
+		var loc4 = new L.LatLng(lat2, lng1);
+
+		feature = L.polyline( [loc1, loc4, loc2, loc3, loc1], {color: 'red'}).addTo(map);
+		map.fitBounds(bounds);
+	}
+}
+
+function addr_search() {
+    var inp = document.getElementById("addr");
+
+    $.getJSON('http://nominatim.openstreetmap.org/search?format=json&limit=5&q=' + inp.value, function(data) {
+        var items = [];
+
+        $.each(data, function(key, val) {
+            bb = val.boundingbox;
+            items.push("<li><a href='#' onclick='chooseAddr(" + bb[0] + ", " + bb[2] + ", " + bb[1] + ", " + bb[3]  + ", \"" + val.osm_type + "\");return false;'>" + val.display_name + '</a></li>');
+        });
+
+		$('#results').empty();
+        if (items.length != 0) {
+            $('<p>', { html: "Search results:" }).appendTo('#results');
+            $('<ul/>', {
+                'class': 'my-new-list',
+                html: items.join('')
+            }).appendTo('#results');
+        } else {
+            $('<p>', { html: "No results found" }).appendTo('#results');
+        }
+    });
+}
+
+$(document).ready(function() {
+    load_map();
+
+    /* pagina de referencia para móvil: http://leafletjs.com/examples/mobile.html */
+    map.locate({setView: true, maxZoom: 16});
+
+    function onLocationFound(e) {
+      var radius = e.accuracy / 2;
+      L.marker(e.latlng).addTo(map).bindPopup("You are within " + radius + " meters from this point").openPopup();
+      L.circle(e.latlng, radius).addTo(map);
+    }
+
+    map.on('locationfound', onLocationFound);
+
+    function onLocationError(e) {
+      alert(e.message);
+    }
+    map.on('locationerror', onLocationError);
+
+    var popup = L.popup();
+    function onMapClick(e) {
+       var coor = e.latlng;
+       console.log(coor);
+       popup.setLatLng(e.latlng).setContent("Ubicación: " + e.latlng.toString()).openOn(map);
+       var circle = L.circle(coor, 500, {
+           color: 'red',
+           fillColor: '#f03',
+           fillOpacity: 0.5
+       }).addTo(map);
+    }
+    map.on('click', onMapClick);
+
+});
